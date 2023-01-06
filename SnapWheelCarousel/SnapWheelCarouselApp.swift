790389818358d64b3ee7c0b398a6b73ba662f545
parkks.free <parkks.free@gmail.com>
2023-01-06 09:06:13 +0900

    Initial Commit

---
+++ SnapWheelCarousel/SnapWheelCarouselApp.swift
@@ -0,0 +1,17 @@
+//
+//  SnapWheelCarouselApp.swift
+//  SnapWheelCarousel
+//
+//  Created by パク・ギョンソク on 2023/01/06.
+//
+
+import SwiftUI
+
+@main
+struct SnapWheelCarouselApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
